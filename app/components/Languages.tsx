359e3e86f08123e1494392a78136a33b00f117c0
Tino Zhabinskiy <hey@tino.works>
2023-09-22 17:06:11 -0300

    Fix Languages section

--- app/components/Languages.tsx
+++ app/components/Languages.tsx
@@ -1,16 +1,22 @@
 export default function Languages() {
   return (
     <section id="languages">
-      <h2 className="font-semibold mb-4">Languages</h2>
+      <h2 className="font-semibold mb-4">Design & development</h2>
       <p className="mb-4 leading-relaxed">
-        I&apos;ve been always going back and forth between multiple programming
-        languages. I wasn&apos;t intentionally trying to fit into full-stack
-        role, but that&apos;s definitely where I found myself eventually.
+        I&apos;ve been always going back and forth between design and
+        development throughout my career, until I realized that I don&apos;t
+        have to choose one over another. It&apos;s just simply how I see the
+        modern tech: the one can&apos;t survive without the other.
+      </p>
+      <p className="mb-4 leading-relaxed">
+        For the languages and technologies, I wasn&apos;t intentionally trying
+        to fit into full-stack role, but that&apos;s definitely where I found
+        myself eventually.
       </p>
       <p className="mb-28 leading-relaxed">
         Languages and different frameworks I&apos;ve worked with: Typescript,
-        Javascript, React, React Native, Next, Ruby on Rails, Python, Django,
-        Laravel, Symfony, Swift, SwiftUI.
+        Javascript, React, React Native, Next, Node.js, Express.js, Ruby on
+        Rails, Python, Django, Laravel, Symfony, Swift, SwiftUI.
       </p>
     </section>
   )
