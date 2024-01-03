1bd572b92c189b7f7c724ad0c5618e120b77bf99
Tino Zhabinskiy <hey@tino.works>
2024-01-03 20:03:52 -0300

    v2

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Nav from './components/Nav'
+import Intro from './components/Intro'
 import About from './components/About'
 import Code from './components/Code'
 import Languages from './components/Languages'
@@ -7,11 +8,12 @@ import Motion from './components/Motion'
 export default function Home() {
   return (
     <div className="relative flex flex-col items-start lg:flex-row-reverse container mx-auto w-full lg:w-2/3 xl:w-1/2 py-16 sm:py-32 lg:py-16  px-8 sm:px-12 lg:px-0">
-      <Nav />
+      {/* <Nav /> */}
       <main className="flex flex-col flex-1 lg:pr-16">
-        <About />
-        <Code />
-        <Languages />
+        <Intro />
+        {/* <About /> */}
+        {/* <Code /> */}
+        {/* <Languages /> */}
         <Motion />
       </main>
       <div className="fixed top-0 z-10 bg-gradient-to-b from-white from-35% w-full h-28 -ml-4" />
